1a1d57e826667fe5b6c87134512c8c2fff549103
Dylan Crawshaw <dcrawsh@gmail.com>
2021-01-06 17:47:17 -0800

    linear search

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('algorithms');
+// Search Algorithms 
+function linearSearch(arr, target) {
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i] === target){
+            return i
+        }
+    }
+}
+
+const arr = [5,3,10,-10,33,51];
+
+console.log(linearSearch(arr,10));
